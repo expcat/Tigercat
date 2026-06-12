72727378241567843283cdc937bfc52fc646d92a
expcat <holysdking@hotmail.com>
2026-06-13 00:45:24 +0800

    传递插槽到数据表组件

--- packages/vue/src/components/DataTableWithToolbar.ts
+++ packages/vue/src/components/DataTableWithToolbar.ts
@@ -782,7 +782,7 @@ export const DataTableWithToolbar = defineComponent({
           style: wrapperStyle.value,
           'data-tiger-data-table-with-toolbar': ''
         },
-        [renderToolbar(), h(Table as unknown as Component, tableProps)]
+        [renderToolbar(), h(Table as unknown as Component, tableProps, slots)]
       )
     }
   }
